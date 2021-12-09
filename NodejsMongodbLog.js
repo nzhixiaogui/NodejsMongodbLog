09abfdd0e89bb3ef9e5a7943c22053b5486fabdf
nzhixiaogui <3539002469@qq.com>
2021-12-08 17:01:53 -0800

    123

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app=express()
+app.use('/',express.static('public'))
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send(req.query)
+    console.log(req.query)
 })
 app.listen(0815)
